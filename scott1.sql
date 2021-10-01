e2831f49c025c2030bc87c77295922a480211422
park <parkgod97@naver.com>
2021-10-01 16:54:33 +0900

    ddl 추가

--- scott1.sql
+++ scott1.sql
@@ -1299,3 +1299,88 @@ commit;
 select * from dept_tcl;
 
 select * from dept_tcl;
+
+
+--DDL(데이터 정의어)
+-- 객체를 생성,변경,삭제
+--실행하면 반영됨
+
+-- create( 생성),alter(변경),drop(삭제)
+
+--create table 테이블이름(
+-- 필드명1 자료형,
+-- 필드명2 자려향,
+--)
+
+--테이블 이름 규칙
+--문자로 시작
+--같은 사용자 안에서 동일한 테이블 명 사용불가
+--sql 키워드는 테이블 이름으로 사용할 수 없음
+
+--필드명 생성 규칙
+--문자로 시작
+-- 열 이름은 중복되면 안됨
+--sql 키워드 사용불가
+
+--자료형 : 숫자 NUMBER,문자 varchar2, 날짜 date
+
+create table EMP_DDL(
+    EMPNO NUMBER(4),-- 숫자로 4자리 허용
+    ENAME VARCHAR2(10),
+    JOB VARCHAR2(9),
+    MGR NUMBER(4),
+    HIREDATE DATE,
+    SAL NUMBER(7,2), --숫자로 7자리 허용(소수점 2자리 허용)
+    COMM NUMBER(7,2),
+    DEPTNO NUMBER(2)
+);
+DROP TABLE EMP_DDL;
+--구조+데이터 이용하여생성
+CREATE TABLE DEPT_DDL AS SELECT * FROM DEPT;
+CREATE TABLE DEPT_DDL_30 AS SELECT * FROM DEPT WHERE DEPTNO=30;
+--다른 테이블의 구조만 복사하여 새 테이블 생성
+CREATE TABLE DEPT_DDL2 AS SELECT * FROM DEPT WHERE 1<>1;
+SELECT * FROM DEPT_DDL2;
+
+--ALTER
+--이미 생성된 객체를 변경 
+
+CREATE TABLE EMP_ALTER AS SELECT * FROM EMP;
+
+--새로운 컬럼(열) 추가:ADD
+desc emp_alter;
+
+alter table EMP_ALTER ADD HP VARCHAR2(20);
+SELECT * FROM EMP_ALTER;
+
+--RENAME:열 이름을 변경
+ALTER TABLE EMP_ALTER RENAME COLUMN HP TO TEL;
+
+--MODIFY: 열의 자료형을 변경
+ALTER TABLE EMP_ALTER MODIFY EMPNO NUMBER(5);
+
+--DROP :열 삭제
+ALTER TABLE EMP_ALTER DROP COLUMN TEL;
+
+--테이블 이름 변경
+RENAME EMP_ALTER TO EMP_RENAME;
+DESC EMP_RENAME;
+
+--테이블 데이터를 삭제 TRUNCATE
+SELECT * FROM EMP_RENAME;
+TRUNCATE TABLE EMP_RENAME;
+
+--MEMBER 테이블작성\
+CREATE TABLE MEMBER(
+    ID CHAR(8),
+    NAME VARCHAR2(10),
+    ADDR VARCHAR2(50),
+    NATION CHAR(4),
+    EMAIL VARCHAR(50),
+    AGE NUMBER(7,2)
+);
+
+ALTER TABLE  MEMBER ADD BIGO VARCHAR2(20);
+ALTER TABLE  MEMBER MODIFY BIGO VARCHAR2(30);
+ALTER TABLE  MEMBER RENAME COLUMN BIGO TO REMARK;
+DESC MEMBER;
